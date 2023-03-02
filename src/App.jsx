e9d96a2e33ee498bf1617c6a1de0812c6bbc2ac1
Muhammad Suleman Hamza <suleman.hamza@royalcyber.com>
2023-03-02 19:42:33 +0500

    corrected import files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
-import Banner from "./components/Banner";
-import BelowSwiperSection from "./components/BelowSwiperSection";
-import BestSellerSection from "./components/BestSellerSection";
-import Footer from "./components/footer/Footer";
-import Header from "./components/Header";
-import HomeBody from "./components/HomeBody";
-import HomeSwiper from "./components/HomeSwiper";
-import PopularSection from "./components/popular_section/PopularSection";
-import ProductsSlider from "./components/ProductsSlider";
+// import Banner from "./components/Banner";
+// import BelowSwiperSection from "./components/BelowSwiperSection";
+// import BestSellerSection from "./components/BestSellerSection";
+// import Footer from "./components/footer/Footer";
+// import Header from "./components/Header";
+// import HomeBody from "./components/HomeBody";
+// import HomeSwiper from "./components/HomeSwiper";
+// import PopularSection from "./components/popular_section/PopularSection";
+// import ProductsSlider from "./components/ProductsSlider";
+
+import ContectUs from "./pages/ContactUs" // This import was missing
+
 
 
 function App() {
@@ -15,7 +18,7 @@ function App() {
   return (
     <div className="App">
       <ContectUs/>
-      
+    
     </div>
   )
 }
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 const ContectUs = () => {
   return (
+
+    
     <div className='container border'
     style={{marginTop:"50px",
     width:'50%',
-    backgroundImage:url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fsports-background&psig=AOvVaw3E2KDjVrapI6LQINZ8mkUb&ust=1677786087482000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOj2yf6-u_0CFQAAAAAdAAAAABAE ',)   
-
-    }}>
+    // The following line was causing problem
+    // backgroundImage:url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fsports-background&psig=AOvVaw3E2KDjVrapI6LQINZ8mkUb&ust=1677786087482000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOj2yf6-u_0CFQAAAAAdAAAAABAE ',)   
+    }}
+    
+    >
         <h1>Contact Us</h1>
         <form>
             <label>Name</label>
@@ -19,21 +23,12 @@ const ContectUs = () => {
             <label>NMessage</label>
             <textarea name= 'Your Message' rows='4'/>
             <input type='submit'value="send"/>
-
-
         </form>
     </div>
 
 
-
-
-
-
-
-
-
-
-
-
   )
 }
+
+// this line was missing 
+export default ContectUs
